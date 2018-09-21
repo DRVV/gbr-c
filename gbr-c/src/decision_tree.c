400b7c8415072b3ea757de68e26761796a382d7c
ns <naoki.shimode.dp@hitachi.com>
2018-09-21 19:21:23 +0900

    splitting into multiple sources... (not yet done)

--- gbr-c/src/decision_tree.c
+++ gbr-c/src/decision_tree.c
@@ -44,51 +44,7 @@ typedef struct {
 int comp_feat_dim = 0;
 
 
-int main(int argc, char** argv) {
-  
-  char* path = argv[1];
-  FILE* fp = fopen(path, "r");
-  shape shp = shape_of(fp);
 
-  size_t n_rows = shp.n_rows;
-  size_t n_cols = shp.n_columns;
-  fclose(fp);
-
-  size_t len_data = n_rows;
-
-  int n_features = n_cols - DIM_TARGETS;
-  sample* training_samples = make_samples_from_csv(path);
-  print_samples(training_samples, n_rows, n_features);
-
-  double** input_features = malloc(n_rows * sizeof(double*));
-  get_features(training_samples, n_rows, input_features);
-
-  // initialise nodes
-  node tree[NUM_NODES];
-  init_tree(tree, NUM_NODES);
-
-  ///// debug /////
-  puts("Init nodes:");
-  print_tree(tree, NUM_NODES);
-
-  puts("Fitting...");
-  fit(tree, training_samples, len_data, n_features);
-  // sort input data
-
-  /* output result tree data */
-  puts("Fit result:");
-  print_tree(tree, NUM_NODES);
-
-  halt();
-
-  puts("Prediction");
-  double* result = malloc(n_rows * sizeof(double));
-  
-  predict(tree, NUM_NODES, input_features, result, len_data);
-
-  print_double_array(result, len_data);
-  return 0;
-}
 
 void fit(node* tree, sample* training_samples, size_t len_data, size_t n_features) {
   grow_tree(tree, 0, training_samples, len_data, n_features, 0, len_data); // the initial node is 0, and the inital index is 0.
